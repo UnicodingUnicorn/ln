197905bded3ebcf78b54665f71147476c2d2ef23
UnicodingUnicorn <7555ic@gmail.com>
2017-12-20 00:31:49 +0800

    Prevent storing of repeats in fileserve

--- files/index.js
+++ files/index.js
@@ -18,7 +18,16 @@ var mClient = new minio.Client({
   secretKey : process.env.MINIO_SECRET_KEY
 });
 
+var redis = require("redis");
+var filesCache = redis.createClient({
+  host : 'redis',
+  port : 6379,
+  db : 3
+});
+
 var colours = require("colors");
+var crypto = require("crypto");
+var streamifier = require("streamifier");
 var uniqid = require("uniqid");
 
 var app = express();
@@ -45,9 +54,8 @@ app.post('/file', upload.single('file'), function(req, res){
       message : "File not found"
     });
   }else{
-    var putFile = (user, file) => { //Common function so the same code need not exist twice in this file
+    var putFile = (user, file, id) => { //Common function so the same code need not exist twice in this file
       //Give inserted file random key + extension
-      var id = uniqid() + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1];
       mClient.putObject(user, id, file.buffer, (put_err, etag) => {
         if(put_err){
           console.log(put_err);
@@ -63,20 +71,46 @@ app.post('/file', upload.single('file'), function(req, res){
         }
       });
     };
-    mClient.bucketExists(user, (exists_err) => {
-      if(exists_err){ //Create user bucket if it doesn't exist
-        mClient.makeBucket(user, 'ap-southeast-1', (make_err) => {
-          if(make_err){
-            res.status(500).json({
-              message : "Error storing file"
-            });
-          }else{
-            putFile(user, req.file);
-          }
-        });
-      }else{
-        putFile(user, req.file);
-      }
+    //Compute file's hash and checks for presence in cache.
+    var rs = streamifier.createReadStream(req.file.buffer);
+    var shasum = crypto.createHash('sha256');
+    rs.on('data', (data) => {
+      shasum.update(data);
+    });
+    rs.on('end', () => {
+      var hash = shasum.digest('hex');
+      filesCache.get(hash, (get_err, file_users) => { //Check cache for existence of hash
+        if(file_users)
+          file_users = JSON.parse(file_users);
+        if(file_users && Object.keys(file_users).includes(user)){
+          res.status(200).json({
+            message : "Success",
+            filename : file_users[user],
+            originalname : req.file.originalname
+          });
+        }else{
+          if(!file_users)
+            file_users = {};
+          var id = uniqid() + '.' + req.file.originalname.split('.')[req.file.originalname.split('.').length - 1];
+          file_users[user] = id;
+          filesCache.set(hash, JSON.stringify(file_users));
+          mClient.bucketExists(user, (exists_err) => {
+            if(exists_err){ //Create user bucket if it doesn't exist
+              mClient.makeBucket(user, 'ap-southeast-1', (make_err) => {
+                if(make_err){
+                  res.status(500).json({
+                    message : "Error storing file"
+                  });
+                }else{
+                  putFile(user, req.file, id);
+                }
+              });
+            }else{
+              putFile(user, req.file, id);
+            }
+          });
+        }
+      });
     });
   }
 });
